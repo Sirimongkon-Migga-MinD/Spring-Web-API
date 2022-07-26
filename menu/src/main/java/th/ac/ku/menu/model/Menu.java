c8a23e11edb67b3ecebf8d35b12f757e40fae96a
Teptat Tangsajakul <teptat.t@ku.th>
2022-07-26 16:07:09 +0700

    Spring Web API

---
+++ menu/src/main/java/th/ac/ku/menu/model/Menu.java
@@ -0,0 +1,54 @@
+package th.ac.ku.menu.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Menu {
+
+    @Id //int ไม่ค่อยปลอกภัย เพราะเจนเป็นลำดับ
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+    private double price;
+    private String category;
+
+    //  ให้ Generate..
+    //       - Getters และ Setters ทั้งหมด
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
+
